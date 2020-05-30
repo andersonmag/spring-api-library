c05b28e127c0e50422571afb4cb1aa04a3dc7fdc
Anderson <adnrepper@gmail.com>
2020-05-30 15:05:37 -0300

    Repositorio de Categoria

---
+++ src/main/java/com/api/library/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.api.library.repository;
+
+import com.api.library.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+    
+}
