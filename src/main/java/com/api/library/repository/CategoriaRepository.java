a8a2ab572f71b490a9685b187edf4f52d7ea3b54
Anderson <adnrepper@gmail.com>
2020-06-02 13:27:03 -0300

    Metodo buscar por link

--- src/main/java/com/api/library/repository/CategoriaRepository.java
+++ src/main/java/com/api/library/repository/CategoriaRepository.java
@@ -1,10 +1,12 @@
 package com.api.library.repository;
 
+import java.util.Optional;
+
 import com.api.library.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-    
+    Optional<Categoria> findByLink(String link);
 }
