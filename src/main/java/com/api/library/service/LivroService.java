d17c48f3abb47833ae8dd55684aab6fc953fb68e
Anderson <adnrepper@gmail.com>
2020-06-10 22:36:32 -0300

    Metodos CRUD criados no Service de Livros

--- src/main/java/com/api/library/service/LivroService.java
+++ src/main/java/com/api/library/service/LivroService.java
@@ -1,5 +1,7 @@
 package com.api.library.service;
 
+import java.util.Optional;
+import com.api.library.model.Categoria;
 import com.api.library.model.Livro;
 import com.api.library.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +16,29 @@ public class LivroService {
     private LivroRepository livroRepository;
 
     public Page<Livro> obterTodos(Pageable pageable, String titulo) {
-        
+
         if (titulo != null)
             return livroRepository.findByTituloContainingIgnoreCase(titulo, pageable);
         return livroRepository.findAll(pageable);
+    }
+
+    public Page<Livro> obterPorCategoria(Categoria categoria, Pageable pageable) {
+       return livroRepository.findByCategoria(categoria, pageable);
+    }
+
+    public Optional<Livro> obterPorLink(String link) {
+       return livroRepository.findByLink(link);
+    }
+
+    public Optional<Livro> obterPorId(Long id) {
+       return livroRepository.findById(id);
+    }
+
+    public Livro salvar(Livro livro) {
+        return livroRepository.save(livro);
+    }
+
+    public void excluir(Livro livro) {
+        livroRepository.delete(livro);
     }
 }
