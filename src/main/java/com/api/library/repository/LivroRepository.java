f292a6d4ed033d8f99bb49249e371883f65a7b76
Anderson <adnrepper@gmail.com>
2020-07-03 23:18:56 -0300

    Repositorios de Usuario e Pedido criados

--- src/main/java/com/api/library/repository/LivroRepository.java
+++ src/main/java/com/api/library/repository/LivroRepository.java
@@ -1,11 +1,8 @@
 package com.api.library.repository;
 
-import java.util.List;
 import java.util.Optional;
-
 import com.api.library.model.Categoria;
 import com.api.library.model.Livro;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/api/library/repository/UsuarioRepository.java
+++ src/main/java/com/api/library/repository/UsuarioRepository.java
@@ -1,11 +1,11 @@
 package com.api.library.repository;
 
 import java.util.Optional;
-import com.api.library.model.Categoria;
+import com.api.library.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
-    Optional<Categoria> findByLink(String link);
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
