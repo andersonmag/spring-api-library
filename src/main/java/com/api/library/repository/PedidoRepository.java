8124e3b1e3b76a0286939a21aa106c9d955c2b74
Anderson <adnrepper@gmail.com>
2020-07-04 22:49:42 -0300

    Metodo de busca por codigo

--- src/main/java/com/api/library/repository/PedidoRepository.java
+++ src/main/java/com/api/library/repository/PedidoRepository.java
@@ -1,8 +1,12 @@
 package com.api.library.repository;
 
+import java.util.Optional;
+
 import com.api.library.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    Optional<Pedido> findByCodigo(int codigo);
+}
