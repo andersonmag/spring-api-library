40d75d603135d0541a57b5350d26f94cab2631f5
Anderson <adnrepper@gmail.com>
2020-07-04 22:53:16 -0300

    Classe Service de Usuario

--- src/main/java/com/api/library/service/PedidoService.java
+++ src/main/java/com/api/library/service/PedidoService.java
@@ -1,10 +1,14 @@
 package com.api.library.service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Random;
 import com.api.library.model.Pedido;
+import com.api.library.model.Usuario;
 import com.api.library.repository.PedidoRepository;
+import com.api.library.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +16,13 @@ public class PedidoService {
 
     @Autowired
     private PedidoRepository pedidoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Optional<Pedido> obterPorCodigo(int codigo) {
+        return pedidoRepository.findByCodigo(codigo);
+    }
 
     public Pedido salvar(Pedido pedido) {
 
@@ -28,6 +39,9 @@ public class PedidoService {
             }
         } while (codigoRepetido);
 
+        Usuario usuarioPedido = usuarioRepository.findById(pedido.getUsuario().getId())
+                                                        .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+        pedido.setUsuario(usuarioPedido);
         pedido.setCodigo(codigo);
         pedido.setData(LocalDateTime.now());
 
@@ -40,10 +54,9 @@ public class PedidoService {
         int codigo = 0;
 
         for (int i = 0; i <= totalIt; i++) {
-             codigo += Integer.parseInt(random.nextInt((10000 - 1000) + 1) + 1000 + "" );
+            codigo += Integer.parseInt(random.nextInt((10000 - 1000) + 1) + 1000 + "");
         }
 
         return codigo;
     }
-
 }
---
+++ src/main/java/com/api/library/service/UsuarioService.java
@@ -0,0 +1,31 @@
+package com.api.library.service;
+
+import java.util.List;
+import java.util.Optional;
+import com.api.library.model.Usuario;
+import com.api.library.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    
+    public List<Usuario> obterTodos() {
+        return this.usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> obterPorId(Long id) {
+       return usuarioRepository.findById(id);
+    }
+
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void excluir(Usuario usuario) {
+        usuarioRepository.delete(usuario);
+    }
+}
