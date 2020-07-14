dbb0d5da9be15645e5007b7e17594af981474630
Anderson <adnrepper@gmail.com>
2020-07-14 18:42:53 -0300

    Classe para carregar beans do Sistema

---
+++ src/main/java/com/api/library/ApplicationContextLoad.java
@@ -0,0 +1,23 @@
+package com.api.library;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextLoad implements ApplicationContextAware {
+
+    @Autowired
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextLoad.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+}
