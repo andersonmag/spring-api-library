93007992963cb809137423306c6e6e1e5eb065dd
Anderson <adnrepper@gmail.com>
2020-07-14 18:45:35 -0300

    Configuração de Segurança inicial, com mapeamentos, filtros, definição de login por Token e resolução de erro com URL do H2

--- src/main/java/com/api/library/security/WebSecurityConfig.java
+++ src/main/java/com/api/library/security/WebSecurityConfig.java
@@ -1,26 +1,44 @@
 package com.api.library.security;
 
+import com.api.library.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter   {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetails;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetails).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.headers().frameOptions().disable().and()
-        .csrf().disable()
-        .authorizeRequests().antMatchers("/api/**").permitAll();
-    }
+     
+        http
+            .headers().frameOptions().disable() //configuração pro h2 funfar
+            .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .disable().authorizeRequests()
+            .antMatchers("/livros").authenticated()
+            .antMatchers("/**").permitAll()
+            .and().logout().logoutSuccessUrl("/")
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/api/h2/**");
+            .and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                                    UsernamePasswordAuthenticationFilter.class)
+            
+            .addFilterBefore(new JWTApiAuthentication(), UsernamePasswordAuthenticationFilter.class);
     }
-
-    
 }
