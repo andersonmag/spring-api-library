029b11d07ee378e17aae9c7301a3d3300030512c
Anderson <adnrepper@gmail.com>
2020-08-31 19:47:05 -0300

    Mapeamentos GET liberados

--- src/main/java/com/api/library/security/WebSecurityConfig.java
+++ src/main/java/com/api/library/security/WebSecurityConfig.java
@@ -1,27 +1,23 @@
 package com.api.library.security;
 
 import com.api.library.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Component
 @Configuration
 @EnableWebMvc
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
+    @Autowired
     private UserDetailsServiceImpl userDetails;
-
-    public WebSecurityConfig(UserDetailsServiceImpl userDetails) {
-        this.userDetails = userDetails;
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -35,15 +31,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .headers().frameOptions().disable() //configuração pro h2 funfar
         .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
         .disable().authorizeRequests()
-        .antMatchers("/**").permitAll()
-        .and().logout().logoutSuccessUrl("/")
-        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .antMatchers(HttpMethod.GET).permitAll()
+        .anyRequest().authenticated()
 
         .and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
                                 UsernamePasswordAuthenticationFilter.class)
-        
         .addFilterBefore(new JWTApiAuthentication(), UsernamePasswordAuthenticationFilter.class);
     }
-
-
 }
