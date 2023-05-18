dc742ba821a172202aacae7c88b0ff2c075c5334
Anderson <andersondel.dev@gmail.com>
2023-05-18 14:21:31 -0300

    Adiciona mensagem de com numero de caracteres minimos

--- src/main/java/com/api/library/dto/UsuarioRequestDTO.java
+++ src/main/java/com/api/library/dto/UsuarioRequestDTO.java
@@ -4,24 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter
 public class UsuarioRequestDTO {
-
     @NotBlank
     private String nome;
-
     @NotBlank
     @Email
     private String email;
-
     @NotBlank
-    @Min(value = 8)
+    @Length(min = 8, message = "Senha deve ter no mínimo 8 caracteres")
     private String senha;
 }
