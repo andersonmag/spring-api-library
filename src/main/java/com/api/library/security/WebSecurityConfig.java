984cb190bcc29b6ac29c17022ff374ba87a9cf7e
Anderson <andersondel.dev@gmail.com>
2023-05-26 16:45:03 -0300

    Atualiza configuração de segurança setando como @beans do spring

--- src/main/java/com/api/library/security/WebSecurityConfig.java
+++ src/main/java/com/api/library/security/WebSecurityConfig.java
@@ -1,50 +1,72 @@
 package com.api.library.security;
 
-import com.api.library.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Collections;
 
 @AllArgsConstructor
 @Configuration
-@EnableWebMvc
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
     
-    private final UserDetailsServiceImpl userDetails;
     private final JWTTokenAuthenticationService jwtTokenAuthenticationService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetails).passwordEncoder(new BCryptPasswordEncoder());
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetails, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetails);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
 
-        http
-        .cors()
-        .and().csrf().disable()
-                .authorizeRequests()
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider) {
+        return new ProviderManager(Collections.singletonList(authenticationProvider));
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+
+         http
+            .cors()
+            .and()
+            .csrf()
+                 .disable()
+            .authorizeRequests()
                 .antMatchers(HttpMethod.GET).permitAll()
                 .antMatchers(HttpMethod.POST, "/login", "/usuarios").permitAll()
-        .anyRequest().authenticated()
-        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-        .and().addFilterBefore(new JWTLoginFilter("/login", authenticationManager(), jwtTokenAuthenticationService),
-                                UsernamePasswordAuthenticationFilter.class)
-        .addFilterBefore(new JWTApiAuthentication(jwtTokenAuthenticationService), UsernamePasswordAuthenticationFilter.class);
+                .anyRequest().authenticated()
+                .and()
+            .sessionManagement()
+                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                 .and()
+            .addFilterBefore(new JWTLoginFilter("/login", authenticationManager, jwtTokenAuthenticationService),
+                                                 UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(new JWTApiAuthentication(jwtTokenAuthenticationService), UsernamePasswordAuthenticationFilter.class);
+         return http.build();
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/h2/**");
+    @Bean
+    public WebSecurityCustomizer webSecurity(){
+        return (web) -> web.ignoring().antMatchers("/h2/**");
     }
 }
