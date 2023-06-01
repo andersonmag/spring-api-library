2dab08954df8752249703ad806f1c414cd6342a0
Anderson <andersondel.dev@gmail.com>
2023-06-01 14:37:15 -0300

    Corrige o acesso ao Swagger por servidor web

--- src/main/java/com/api/library/config/SwaggerConfig.java
+++ src/main/java/com/api/library/config/SwaggerConfig.java
@@ -5,9 +5,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletContext;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -21,7 +25,7 @@ public class SwaggerConfig {
     }
 
     @Bean
-    public OpenAPI apiInfo() {
+    public OpenAPI apiInfo(ServletContext context) {
         return new OpenAPI()
                 .info(new Info()
                         .title("Spring Library API")
@@ -29,10 +33,11 @@ public class SwaggerConfig {
                         .version("1.0.0")
                         .contact(new Contact()
                                 .name("Anderson")
-                                .email("https://github.com/andersonmag")
-                                .url("andersondel.dev@gmail.com")))
+                                .email("andersondel.dev@gmail.com")
+                                .url("https://github.com/andersonmag")))
                 .components(new Components()
                         .addSecuritySchemes("token-authotization",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+                .servers(List.of(new Server().url(context.getContextPath())));
     }
 }
