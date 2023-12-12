4d27b1d10c1a352ff604836c3620d406cdfddb93
Anderson <andersondel.dev@gmail.com>
2023-12-11 23:48:08 -0300

    Add tratamento de token exceptions

--- src/main/java/com/api/library/exception/EmailExistenteException.java
+++ src/main/java/com/api/library/exception/EmailExistenteException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class EmailExistenteException extends RuntimeException{
+public class EmailExistenteException extends RuntimeException {
 
     public EmailExistenteException(String message) {
         super(message);
